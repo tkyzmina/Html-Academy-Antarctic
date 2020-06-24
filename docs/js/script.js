b57ee62c32b541ff5392351334077792c1aacd9c
tkyzmina <tkyzmina@gmail.com>
2020-06-24 19:10:33 +0300

    добавляет вывод меню в мобильной версии без js

--- docs/js/script.js
+++ docs/js/script.js
@@ -4,14 +4,19 @@
   var menu = document.querySelector('.page-header__nav');
   var menuOpenBtn = document.querySelector('.page-header__menu-toggle');
   var menuCloseBtn = menu.querySelector('.nav__close');
+  var noJs = document.querySelector('.page-header--no-js');
 
-
-  menuOpenBtn.onclick = function () {
-    menu.classList.add('page-header__nav--opened');
-
-  };
-
-  menuCloseBtn.onclick = function () {
+  if (noJs) {
+    noJs.classList.remove('page-header--no-js');
     menu.classList.remove('page-header__nav--opened');
-  };
+
+    menuOpenBtn.onclick = function () {
+      menu.classList.add('page-header__nav--opened');
+    };
+
+    menuCloseBtn.onclick = function () {
+      menu.classList.remove('page-header__nav--opened');
+    };
+  }
+
 })();
