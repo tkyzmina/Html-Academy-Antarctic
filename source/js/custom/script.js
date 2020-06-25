f9c0afd9712eb7950be22785085e9af5d946f8a0
tkyzmina <tkyzmina@gmail.com>
2020-06-25 21:10:52 +0300

    добавляет валидацию поля имя

--- source/js/custom/script.js
+++ source/js/custom/script.js
@@ -5,6 +5,8 @@
   var menuOpenBtn = document.querySelector('.page-header__menu-toggle');
   var menuCloseBtn = menu.querySelector('.nav__close');
   var noJs = document.querySelector('.page-header--no-js');
+  var nameField = document.querySelector('input[name = "user-name"]');
+  var onlyLetters = /^[A-Za-zА-Яа-я]*$/;
 
   if (noJs) {
     noJs.classList.remove('page-header--no-js');
@@ -19,4 +21,18 @@
     };
   }
 
+  nameField.oninput = function () {
+    var validationMessage;
+    var name = document.querySelector('input[name = "user-name"]').value;
+    if (!onlyLetters.test(name)) {
+      validationMessage = 'недопустимые символы! допустимы только буквы';
+    }
+    if (validationMessage) {
+      nameField.setCustomValidity(validationMessage);
+      nameField.style.border = '2px solid red';
+    } else {
+      nameField.setCustomValidity('');
+      nameField.style.border = '';
+    }
+  };
 })();
