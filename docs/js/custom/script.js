87682763dde0315acfd3ceba327a72917919b395
tkyzmina <tkyzmina@gmail.com>
2020-06-30 13:10:46 +0300

    корректирует валилацию  - pattern и загрузку шрифтов

--- docs/js/custom/script.js
+++ docs/js/custom/script.js
@@ -5,8 +5,6 @@
   var menuOpenBtn = document.querySelector('.page-header__menu-toggle');
   var menuCloseBtn = menu.querySelector('.nav__close');
   var noJs = document.querySelector('.page-header--no-js');
-  var nameField = document.querySelector('input[name = "user-name"]');
-  var onlyLetters = /^[A-Za-zА-Яа-я]*$/;
 
   if (noJs) {
     noJs.classList.remove('page-header--no-js');
@@ -20,19 +18,4 @@
       menu.classList.remove('page-header__nav--opened');
     };
   }
-
-  nameField.oninput = function () {
-    var validationMessage;
-    var name = document.querySelector('input[name = "user-name"]').value;
-    if (!onlyLetters.test(name)) {
-      validationMessage = 'недопустимые символы! допустимы только буквы';
-    }
-    if (validationMessage) {
-      nameField.setCustomValidity(validationMessage);
-      nameField.style.border = '2px solid red';
-    } else {
-      nameField.setCustomValidity('');
-      nameField.style.border = '';
-    }
-  };
 })();
